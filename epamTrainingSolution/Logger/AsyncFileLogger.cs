7cdf4060b57f4a2d05bfbfb0ee81d9ed1f446f3f
Roman Lynnyk <roman.lynnyk.sa.@gmail.com>
2019-11-01 12:56:12 +0200

    added exception handlers to 7 and 8 training

--- epamTrainingSolution/Logger/AsyncFileLogger.cs
+++ epamTrainingSolution/Logger/AsyncFileLogger.cs
@@ -6,9 +6,7 @@ using System.Threading.Tasks;
 
 namespace Logger
 {
-    interface ICommonLogger : ILogger
+    class AsyncFileLogger
     {
-        void readMessageLog();
-        void writeMessageLog(Exception exception);
     }
 }
